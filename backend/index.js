c813c68ae9018c3ab00c8bf3771f698f07c6b681
Sathish reddy <104757035+sathish-nalladevagari@users.noreply.github.com>
2024-03-21 12:37:05 +0530

    Created books post and get api to retrive and store books

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,50 @@
 import express from "express";
 import { MONGOURI, PORT } from "./config.js";
 import mongoose from "mongoose";
+import { Book } from "./models/bookModel.js";
 
 const app = express();
+app.use(express.json())
 
 
 app.get("/",(req,res)=>{
     console.log(req)
     return res.status(200).send("Welcome")
+})
+
+app.post("/books",async(req,res)=>{
+    // console.log(req.body)
+    try {
+        if (!req.body.title || !req.body.author || !req.body.publishYear ){
+            return res.status(400).send("Send Author, title, publishYear")
+        }
+        const newBook = {
+            title : req.body.title,
+            author : req.body.author,
+            publishYear : req.body.publishYear
+        }
+        const book = await Book.create(newBook)
+        return res.status(201).send(book)
+
+    } 
+    catch (error) {
+        console.log(error)
+    }
+})
+
+app.get("/books",async (req,res)=>{
+    try {
+        const books = await Book.find({})
+        if (!books){
+            return res.status(401).send("No Books Detected")
+        }
+        return res.status(201).json({
+            count : books.length,
+            data: books 
+        })
+    } catch (error) {
+        return res.status(400).send({message:error.message})
+    }
 })
 
 
