6d0a9af4e6caf8f6643cffc49a9c456ff3bc89fe
Sathish reddy <104757035+sathish-nalladevagari@users.noreply.github.com>
2024-03-21 11:27:40 +0530

    first commit

---
+++ backend/index.js
@@ -0,0 +1,17 @@
+import express from "express";
+import { PORT } from "./config.js";
+
+
+const app = express();
+
+
+app.get("/",(req,res)=>{
+    console.log(req)
+    return res.status(200).send("Welcome")
+})
+
+
+app.listen(PORT,()=>{
+    console.log(`server started at ${PORT}`)
+})
+
